739a9ed55ea4d3815abbdf2edfcbed0646e62f17
Allen Valdez <AllenValdez1728@gmail.com>
2023-05-12 22:05:44 -0700

    Separated content into smaller components

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,16 @@
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './Expenses.css';
+
+const Expenses = ({expenses}) => {
+	return (
+		<Card className="expenses">
+			<ExpenseItem expenses={expenses[0]} />
+			<ExpenseItem expenses={expenses[1]} />
+			<ExpenseItem expenses={expenses[2]} />
+			<ExpenseItem expenses={expenses[3]} />
+		</Card>
+	);
+};
+
+export default Expenses;
